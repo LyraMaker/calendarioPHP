d76c217bedc753a5b57a050cfade738546df45a7
LyraMaker <gabriel.lyrat@gmail.com>
2021-10-17 23:45:20 -0300

    Finalizada a contrução total do do calendário

--- estruturas/calendario.php
+++ estruturas/calendario.php
@@ -1,17 +1,38 @@
 <?php
 
-function tituloCalendario(){
-return "<thead><tr class='has-text-centered has-background-primary'><th>DOM</th><th>SEG</th><th>TER</th><th>QUA</th><th>QUI</th><th>SEX</th><th>SÁB</th></tr></thead>";}
+function calendario()
+{
+    $calendario = '';
+    $dia = 1;
+    $semana = [];
 
-function gerarLinha(){
-    return "
-    <tr class='has-text-centered'>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-    </tr>";
+    while ($dia <= 31) {
+        array_push($semana, $dia);
+
+        if (count($semana) == 7) {
+            $calendario .= gerarLinha($semana);
+            $semana = [];
+        }
+        $dia++;
+    }
+    $calendario .= gerarLinha($semana);
+
+    return $calendario;
+}
+
+function tituloCalendario()
+{
+    return "<thead><tr class='has-text-centered has-background-primary'><th>DOM</th><th>SEG</th><th>TER</th><th>QUA</th><th>QUI</th><th>SEX</th><th>SÁB</th></tr></thead>";
+}
+
+function gerarLinha($semana)
+{
+    $linha = "<tr>";
+
+    for ($diaSemana = 0; $diaSemana <= 6; $diaSemana++) {
+        array_key_exists($diaSemana, $semana) ? $linha .= "<td>{$semana[$diaSemana]}</td>" : $linha .= "<td></td>"; //Optei utilizar um operador ternário pois é uma condição composta de estrutura simples
+    }
+    $linha .= "</tr>";
+
+    return $linha;
 }
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php 
-    include "confi/ambiente.php";
-    include 'estruturas/calendario.php';
-    setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+<?php
+include "confi/ambiente.php";
+include 'estruturas/calendario.php';
+setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
 date_default_timezone_set('America/Sao_Paulo');
 
 ?>
@@ -20,27 +20,29 @@ date_default_timezone_set('America/Sao_Paulo');
     <header class="has-background-info">
         <div class="container has-text-centered p-4">
             <h1 class="title has-text-white	 ">📅 calendarioPHP | Que dia é hoje ?</h1>
-            <h2 class="subtitle pt-3 has-text-white"><?php echo	"Hoje é "."<strong class='has-text-warning'>".date('d/m/Y').'</strong>';	?> </h2>
+            <h2 class="subtitle pt-3 has-text-white"><?php echo    "Hoje é " . "<strong class='has-text-warning'>" . date('d/m/Y') . '</strong>';    ?> </h2>
         </div>
     </header>
     <main class="section">
-       <div class="container is-flex is-justify-content-center">
-           <table class="table is-striped is-bordered">
-               <?php echo tituloCalendario(); 
-               ?>
-               <tbody>
-               <?php
-               for($i = 0;$i<=4;$i++){
-                echo gerarLinha();
-               }
-               ?>
-            </tbody>
-           </table>
-       </div>
+        <div class="container is-flex is-justify-content-center">
+            <table class="table is-striped is-bordered">
+                <?php echo tituloCalendario();
+                ?>
+                <tbody>
+                    <?php
+                    echo calendario();
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </main>
     <footer class="p-4 has-text-white is-flex is-justify-content-center has-background-black">
-        <div class="container has-text-centered"><p>Site desenvolvido por <em>Gabriel Lyra Torquato</em></p></div>
-        <div class="container has-text-centered"><p>Página gerada às <?php echo date('H').':'.date('i'); ?></p></div>
+        <div class="container has-text-centered">
+            <p>Site desenvolvido por <em>Gabriel Lyra Torquato</em></p>
+        </div>
+        <div class="container has-text-centered">
+            <p>Página gerada às <?php echo date('H') . ':' . date('i'); ?></p>
+        </div>
     </footer>
 
 </body>
